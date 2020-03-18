bd40d80dbf546504038cec010a7b6545cab74d48
Lara Clemens <lclemens@uci.edu>
2020-03-17 18:53:36 -0700

    Include itam switch. Plot transitionRates, transitionTimes.

--- src/Plots/GillespieIrreversible/Analysis_IrreversibleGillespie_FilamentSweep.m
+++ src/Plots/GillespieIrreversible/Analysis_IrreversibleGillespie_FilamentSweep.m
@@ -6,15 +6,17 @@ close all;
 overwriteTF = 0;
 %% Initialize Model Choice
 
-spacing = 0; % 0 = CD3Zeta, 1 = EvenSites, 2 = CD3Epsilon, 3 = TCR
+spacing = 3; % 0 = CD3Zeta, 1 = EvenSites, 2 = CD3Epsilon, 3 = TCR
 membrane = 1; % 0 for membrane off, 1 for membrane on
 phos = 1; % 1 = phosphorylation, 0 = dephosphorylation
+itam = 0;
+
 
 % initialization switch for which model we're inspecting
 model = 41; % 1x = stiffening, 2x = electrostatics, 3x = multiple binding - ibEqual
 
 saveRatesPlot = 0;
-
+movieTF = 1;
 %% Model Parameters
 
 savefilefolder = '/Volumes/GoogleDrive/My Drive/Papers/MultisiteDisorder/Data_Figures';
@@ -42,6 +44,13 @@ else
     phosDirection = 'Dephos';
 end
 
+switch (itam)
+    case 0
+        itamLoc = 'End';
+    case 1
+        itamLoc = 'Mid';
+end
+
 switch (model)
     
     case 41 % Filament Sweep - separation distance 17
@@ -52,7 +61,7 @@ switch (model)
         % find files
         filefolder    = '~/Documents/Papers/MultisiteDisorder/Data/3.SimultaneousBinding';
         filesubfolder = [iSiteSpacing,'/Membrane',membraneState,'/FilVSTime/SepDist17/','ITAM_',itamLoc,'/3.Gillespie/Irreversible/','CatFiles/',phosDirection];
-        filetitle     = ['IrreversibleGillespie',spacing,'Membrane',membraneState,phosDirection];
+        filetitle     = ['IrreversibleGillespie',iSiteSpacing,'Membrane',membraneState,phosDirection];
         
         % save figures location
         savefilesubfolder = ['3.SimultaneousBinding/',iSiteSpacing,'/Membrane',membraneState,'/FilVSTime/SepDist17/','ITAM_',itamLoc,'/Plots/',phosDirection];
@@ -115,7 +124,7 @@ if(~exist(fullfile(savefilefolder,savefilesubfolder,'Data.mat'),'file') || overw
             clear M;
             
             % set up filename
-            filename = strcat(filetitle,sweepParameter,num2str(sweep(s)),'.NFIL',NFil,'.AllData')
+            filename = strcat(filetitle,sweepParameter,num2str(sweep(s)),'.NFIL',num2str(NFil),'.AllData')
             
             % read in file
             M = dlmread(fullfile(filefolder,filesubfolder,filename));
@@ -145,6 +154,70 @@ load(fullfile(savefilefolder,savefilesubfolder,'Data.mat'));
 %%%%%%%%%%%%%%%%%%%%%% AVERAGE TRANSITION RATE PLOTS %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fontName = 'Arial';
+fs = 18;
+
+%% Plot transition times
+
+NFilData = repmat(NFilSweep',1,size(transitionTime_Avg,2));
+ITAMLengthData = repmat(sweep,size(transitionTime_Avg,1),1);
+
+figure(1); clf; hold on;
+plotData = reshape(transitionTime_Avg(:,:,6),[size(transitionTime_Avg,1),size(transitionTime_Avg,2)]);
+surf(NFilData,ITAMLengthData,log10(plotData));
+xlabel1 = 'Number Filaments';
+ylabel1 = 'ITAM separation distance';
+zlabel1 = 'Average transition time to ith bound';
+xlabel(xlabel1,'FontName',fontName,'FontSize',fs);
+ylabel(ylabel1,'FontName',fontName,'FontSize',fs);
+zlabel(zlabel1,'FontName',fontName,'FontSize',fs);
+
+
+
+%% Plot transition rates
+NFilData = repmat(NFilSweep',1,size(transitionRate_Avg,2));
+ITAMLengthData = repmat(sweep,size(transitionRate_Avg,1),1);
+
+
+moviename = 'FilSweep_transitionRate_Avg';
+
+if (movieTF)
+   Visual3D = VideoWriter(strcat(moviename,'.avi'));
+   Visual3D.FrameRate = 5;
+   open(Visual3D);
+end
+
+
+
+
+for loc = 1:locationTotal
+    figure(2); clf; hold on; view([-42, 24]);
+    plotData = reshape(transitionRate_Avg(:,:,loc),[size(transitionRate_Avg,1),size(transitionRate_Avg,2)]);
+    surf(NFilData,ITAMLengthData,log10(plotData));
+    xlim([0 max(NFilSweep)]);
+    ylim([0 max(sweep)]);
+    zlim([-15 0]);
+    xlabel1 = 'Number Filaments';
+    ylabel1 = 'ITAM separation distance';
+    zlabel1 = 'Average transition time to ith bound';
+    xlabel(xlabel1,'FontName',fontName,'FontSize',fs);
+    ylabel(ylabel1,'FontName',fontName,'FontSize',fs);
+    zlabel(zlabel1,'FontName',fontName,'FontSize',fs);
+    
+    if (movieTF)
+        frame = getframe(gcf);
+        writeVideo(Visual3D, frame);
+    end
+end
+
+
+% close movie
+if (movieTF)
+    close(Visual3D);
+end
+
+
+%%
 if(0)
     %% Average Transition Rates VS Number of Modified Sites - No Labels
     
