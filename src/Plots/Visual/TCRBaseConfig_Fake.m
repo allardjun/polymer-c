5688da642a100ebd51edb6dfbbc4f34f7764089d
Lara Clemens <lclemens@uci.edu>
2021-08-12 19:01:12 -0700

    update plot style and save location

--- src/Plots/Visual/TCRBaseConfig_Fake.m
+++ src/Plots/Visual/TCRBaseConfig_Fake.m
@@ -1,10 +1,48 @@
 %% Base Config for Fake TCR
 
+saveTF = 1;
+
 NFil = [1 2 3 5 9 10];
-baseSepDist = 5;
-colors = parula(7);
-lw = 2;
-for nf = 1:length(NFil)
-    figure(nf); clf; hold on; axis equal;
-    plot(baseSepDist*cos((0:(NFil(nf)-1)).*2*pi./NFil(nf)),baseSepDist*sin((0:(NFil(nf)-1)).*2*pi./NFil(nf)),'*','LineWidth',lw,'Color',colors(nf,:));
+baseSepDistance = [5,17]; % Kuhn lengths*nm
+
+savefolder = '/Volumes/GoogleDrive/My Drive/Papers/MultisiteDisorder/Data_Figures/3.SimultaneousBinding/TCR/MembraneOn/FilVSTime';
+
+%% Figure parameters
+ms = 7; % marker size
+xm = 6; % x limit in nm
+ym = 6; % y limit in nm
+lw = 2; % line width
+fs = 18; % font size
+
+%% Plot bases configurations
+for bSD = 1:length(baseSepDistance)
+    
+    baseSepDist = baseSepDistance(bSD);
+    savesubfolder = ['SepDist',num2str(baseSepDist),'/ITAM_End/Plots/Visuals'];
+
+    for nf = 1:length(NFil)
+       
+        % figure
+        figure(nf); clf; hold on; box on; axis equal;
+        plot(0.3*baseSepDist*cos((0:(NFil(nf)-1)).*2*pi./NFil(nf)),0.3*baseSepDist*sin((0:(NFil(nf)-1)).*2*pi./NFil(nf)),'xk','LineWidth',lw,'MarkerSize',ms);
+        
+        % axis limits, axes ticks
+        xlim([-xm xm]);
+        ylim([-ym ym]);
+        xticks([-xm -xm/2 0 xm/2 xm]);
+        yticks([-ym -ym/2 0 ym/2 ym]);
+        
+        % set size
+        set(gcf,'units','centimeters','Position',[5 5 3 3]);
+        
+        % save figure
+        if(saveTF)
+            saveas(gcf,fullfile(savefolder,savesubfolder,['TCRBaseConfig_Fake_SepDist',num2str(baseSepDist),'_NFil',num2str(nf),'.fig']),'fig');
+            saveas(gcf,fullfile(savefolder,savesubfolder,['TCRBaseConfig_Fake_SepDist',num2str(baseSepDist),'_NFil',num2str(nf),'.eps']),'epsc');
+        end
+
+    end
+
 end
+
+
